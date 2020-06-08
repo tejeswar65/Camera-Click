808125400cea4c1477a39451883206593793acc8
tejeswar65 <teja.wudaru@gmail.com>
2020-06-08 10:09:23 +0530

    First commit

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {Subject, Observable} from 'rxjs';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+  public DispWebcam = true;
+  public CameraSwitch = true;
+  public videoOptions: MediaTrackConstraints = {};
+  public errors: WebcamInitError[] = [];
+  public webcamImage: WebcamImage = null;
+  private trigger: Subject<void> = new Subject<void>();
+
+  ngOnInit(): void {
+
+  }
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+  public toggleWebcam(): void {
+    this.DispWebcam = !this.DispWebcam;
+  }
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('received webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+  }
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+}
